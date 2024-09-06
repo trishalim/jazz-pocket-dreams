8710b38cbf14ba2195d5fdb080f52856f7994af2
Trisha Lim <hello@trishalim.com>
2024-09-06 19:48:52 +0100

    init jazz app

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createJazzReactContext, DemoAuth } from "jazz-react";
+
+const Jazz = createJazzReactContext({
+  auth: DemoAuth({ appName: "Circular" }),
+  peer: "wss://mesh.jazz.tools/?key=you@example.com", // <- put your email here to get a proper API key later
+});
+export const { useAccount, useCoState } = Jazz;
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Jazz.Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Jazz.Provider>
+)
