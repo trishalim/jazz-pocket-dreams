6c05e210ce66909c37f6c4cea8b7d92e43f12eac
Trisha Lim <hello@trishalim.com>
2025-03-10 17:32:02 +0700

    replicate missing covalue

--- src/components/BookReviewThumbnail.tsx
+++ src/components/BookReviewThumbnail.tsx
@@ -8,7 +8,11 @@ import Link from "next/link";
 import { BookReview } from "../schema";
 
 export function BookReviewThumbnail({ id }: { id: ID<BookReview> }) {
-  const bookReview = useCoState(BookReview, id);
+  const bookReview = useCoState(BookReview, id, {
+    cover: {},
+  });
+
+  console.log(id, bookReview);
 
   if (!bookReview) return <></>;
 
