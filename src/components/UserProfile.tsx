892e93cb13c2386b982a27a7178485fa918d72fd
Trisha Lim <hello@trishalim.com>
2025-03-11 12:10:41 +0700

    add promo to user profile

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/Dropdown";
 import { Heading } from "@/components/Heading";
 import { Icon } from "@/components/Icon";
+import { Promo } from "@/components/Promo";
 import { ShareBooksByMonthDialog } from "@/components/ShareBooksByMonthDialog";
 import { ShareProfileDialog } from "@/components/ShareProfileDialog";
 import { LogoShortIcon } from "@/components/icons/LogoShortIcon";
@@ -125,197 +126,200 @@ export default function UserProfile({ id }: { id: ID<JazzAccount> }) {
   }, [booksBySelectedYear, booksByMonth]);
 
   return (
-    <div className="grid gap-4">
-      {isMobileMenuOpen && (
-        <div
-          className={clsx(
-            "fixed sm:hidden flex right-5 left-5 bottom-5 rounded-full p-2 border z-50 bg-white ease-in-out duration-300 shadow-md",
-            "dark:bg-slate-800",
-          )}
-        >
-          <Button
-            onClick={() => setShowAddBookDialog(true)}
-            variant="primary"
-            size="sm"
-            className="mr-2"
+    <>
+      <div className="grid gap-4 mb-12">
+        {isMobileMenuOpen && (
+          <div
+            className={clsx(
+              "fixed sm:hidden flex right-5 left-5 bottom-5 rounded-full p-2 border z-50 bg-white ease-in-out duration-300 shadow-md",
+              "dark:bg-slate-800",
+            )}
           >
-            Add book
-          </Button>
-          <Button
-            variant="secondary"
-            onClick={() => setShowShareProfileDialog(true)}
-            size="sm"
-          >
-            Share profile
-          </Button>
-
-          <button
-            className="ml-auto p-2"
-            aria-label="Close"
-            onClick={() => setIsMobileMenuOpen(false)}
-          >
-            <Icon name="close" size="sm" />
-          </button>
-        </div>
-      )}
-
-      {canWrite && (
-        <Dropdown>
-          <DropdownButton
-            as="button"
-            className="sm:hidden z-40 bg-white ring-1 ring-slate-950/10 shadow-lg rounded-full fixed bottom-6 right-6 p-2.5 dark:bg-slate-800"
-          >
-            <LogoShortIcon className="size-6 text-purple-600 dark:text-purple-400" />
-          </DropdownButton>
-
-          <DropdownMenu anchor="bottom end">
-            <DropdownItem onClick={() => setShowShareProfileDialog(true)}>
-              Share profile
-            </DropdownItem>
-            <DropdownItem onClick={() => setShowAddBookDialog(true)}>
-              Add book
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      )}
-
-      <div className="sm:flex items-center justify-between">
-        <Heading>{profile?.name}&apos;s book shelf</Heading>
-        {canWrite && (
-          <div className="hidden sm:flex gap-2">
-            <Button
-              variant="secondary"
-              onClick={() => setShowShareProfileDialog(true)}
-            >
-              Share profile
-            </Button>
             <Button
               onClick={() => setShowAddBookDialog(true)}
               variant="primary"
+              size="sm"
+              className="mr-2"
             >
               Add book
             </Button>
+            <Button
+              variant="secondary"
+              onClick={() => setShowShareProfileDialog(true)}
+              size="sm"
+            >
+              Share profile
+            </Button>
+
+            <button
+              className="ml-auto p-2"
+              aria-label="Close"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              <Icon name="close" size="sm" />
+            </button>
           </div>
         )}
-      </div>
 
-      <ShareProfileDialog
-        open={showShareProfileDialog}
-        onClose={() => setShowShareProfileDialog(false)}
-      />
-
-      <ShareBooksByMonthDialog
-        open={showShareBooksByMonthDialog}
-        onClose={() => {
-          setShowShareBooksByMonthDialog(false);
-        }}
-        books={booksByMonthToShare?.books}
-        month={booksByMonthToShare?.month}
-      />
-
-      <AddBookDialog
-        onClose={() => setShowAddBookDialog(false)}
-        open={showAddBookDialog}
-      />
-
-      {booksByYear?.length > 1 && (
-        <div>
-          {booksByYear?.map(({ year }) => (
-            <button
-              type="button"
-              className={clsx(
-                "px-2 py-1 rounded-md text-sm mr-2 hover:text-slate-900",
-                selectedYear == year
-                  ? "bg-slate-100 text-slate-900"
-                  : "text-slate-600",
-              )}
-              key={year}
-              onClick={() => setSelectedYear(year)}
+        {canWrite && (
+          <Dropdown>
+            <DropdownButton
+              as="button"
+              className="sm:hidden z-40 bg-white ring-1 ring-slate-950/10 shadow-lg rounded-full fixed bottom-6 right-6 p-2.5 dark:bg-slate-800"
             >
-              {year}
-            </button>
-          ))}
-        </div>
-      )}
+              <LogoShortIcon className="size-6 text-purple-600 dark:text-purple-400" />
+            </DropdownButton>
 
-      {booksBySelectedYear && (
-        <div className="flex flex-col md:flex-row gap-y-4 gap-x-10 mt-2">
-          {[
-            {
-              value: booksBySelectedYear.books.length,
-              label: "books read this year",
-              icon: "book",
-              iconColor: "text-pink-500",
-            },
-            {
-              value: averageRating,
-              label: "average rating",
-              icon: "star",
-              iconColor: "text-amber-400",
-            },
-            {
-              value: booksPerMonth,
-              label: "books read per month",
-              icon: "month",
-              iconColor: "text-green-600",
-            },
-          ].map(
-            ({ value, label, icon, iconColor }) =>
-              value && (
-                <div key={label} className="flex items-center">
-                  <Icon
-                    name={icon}
-                    className={clsx(iconColor, "mr-3")}
-                    size="md"
-                  />
-                  <p className="font-semibold mr-1.5 text-slate-900 dark:text-slate-200">
-                    {value}
-                  </p>
-                  <p>{label}</p>
-                </div>
-              ),
-          )}
-        </div>
-      )}
+            <DropdownMenu anchor="bottom end">
+              <DropdownItem onClick={() => setShowShareProfileDialog(true)}>
+                Share profile
+              </DropdownItem>
+              <DropdownItem onClick={() => setShowAddBookDialog(true)}>
+                Add book
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        )}
 
-      {booksByMonth?.map(({ month, books }) => (
-        <div key={month} className="flex flex-col mt-8">
-          <div className="flex justify-between items-center pb-2 mb-4 border-b">
-            <h2 className="text-sm">
-              <span className=" font-medium text-slate-900 dark:text-slate-200">
-                {month}
-              </span>{" "}
-              <span>
-                {" "}
-                ({books.length} {books.length === 1 ? "book" : "books"})
-              </span>
-            </h2>
-
-            {!!books.length && (
+        <div className="sm:flex items-center justify-between">
+          <Heading>{profile?.name}&apos;s book shelf</Heading>
+          {canWrite && (
+            <div className="hidden sm:flex gap-2">
               <Button
-                size="sm"
                 variant="secondary"
-                onClick={() => {
-                  setBooksByMonthToShare({ month, books });
-                  setShowShareBooksByMonthDialog(true);
-                }}
+                onClick={() => setShowShareProfileDialog(true)}
               >
-                <Icon name="image" size="xs" />
-                Share
+                Share profile
               </Button>
-            )}
-          </div>
-          {books.length == 0 ? (
-            <p className="text-slate-600">No books read this month.</p>
-          ) : (
-            <div className="grid grid-cols-2 gap-8 sm:grid-cols-4 sm:gap-4">
-              {books.map((bookReview) => (
-                <BookReviewThumbnail key={bookReview.id} id={bookReview.id} />
-              ))}
+              <Button
+                onClick={() => setShowAddBookDialog(true)}
+                variant="primary"
+              >
+                Add book
+              </Button>
             </div>
           )}
         </div>
-      ))}
-    </div>
+
+        <ShareProfileDialog
+          open={showShareProfileDialog}
+          onClose={() => setShowShareProfileDialog(false)}
+        />
+
+        <ShareBooksByMonthDialog
+          open={showShareBooksByMonthDialog}
+          onClose={() => {
+            setShowShareBooksByMonthDialog(false);
+          }}
+          books={booksByMonthToShare?.books}
+          month={booksByMonthToShare?.month}
+        />
+
+        <AddBookDialog
+          onClose={() => setShowAddBookDialog(false)}
+          open={showAddBookDialog}
+        />
+
+        {booksByYear?.length > 1 && (
+          <div>
+            {booksByYear?.map(({ year }) => (
+              <button
+                type="button"
+                className={clsx(
+                  "px-2 py-1 rounded-md text-sm mr-2 hover:text-slate-900",
+                  selectedYear == year
+                    ? "bg-slate-100 text-slate-900"
+                    : "text-slate-600",
+                )}
+                key={year}
+                onClick={() => setSelectedYear(year)}
+              >
+                {year}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {booksBySelectedYear && (
+          <div className="flex flex-col md:flex-row gap-y-4 gap-x-10 mt-2">
+            {[
+              {
+                value: booksBySelectedYear.books.length,
+                label: "books read this year",
+                icon: "book",
+                iconColor: "text-pink-500",
+              },
+              {
+                value: averageRating,
+                label: "average rating",
+                icon: "star",
+                iconColor: "text-amber-400",
+              },
+              {
+                value: booksPerMonth,
+                label: "books read per month",
+                icon: "month",
+                iconColor: "text-green-600",
+              },
+            ].map(
+              ({ value, label, icon, iconColor }) =>
+                value && (
+                  <div key={label} className="flex items-center">
+                    <Icon
+                      name={icon}
+                      className={clsx(iconColor, "mr-3")}
+                      size="md"
+                    />
+                    <p className="font-semibold mr-1.5 text-slate-900 dark:text-slate-200">
+                      {value}
+                    </p>
+                    <p>{label}</p>
+                  </div>
+                ),
+            )}
+          </div>
+        )}
+
+        {booksByMonth?.map(({ month, books }) => (
+          <div key={month} className="flex flex-col mt-8">
+            <div className="flex justify-between items-center pb-2 mb-4 border-b">
+              <h2 className="text-sm">
+                <span className=" font-medium text-slate-900 dark:text-slate-200">
+                  {month}
+                </span>{" "}
+                <span>
+                  {" "}
+                  ({books.length} {books.length === 1 ? "book" : "books"})
+                </span>
+              </h2>
+
+              {!!books.length && (
+                <Button
+                  size="sm"
+                  variant="secondary"
+                  onClick={() => {
+                    setBooksByMonthToShare({ month, books });
+                    setShowShareBooksByMonthDialog(true);
+                  }}
+                >
+                  <Icon name="image" size="xs" />
+                  Share
+                </Button>
+              )}
+            </div>
+            {books.length == 0 ? (
+              <p className="text-slate-600">No books read this month.</p>
+            ) : (
+              <div className="grid grid-cols-2 gap-8 sm:grid-cols-4 sm:gap-4">
+                {books.map((bookReview) => (
+                  <BookReviewThumbnail key={bookReview.id} id={bookReview.id} />
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <Promo />
+    </>
   );
 }
